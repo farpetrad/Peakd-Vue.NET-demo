d1e716ab9bf4f00bf8fa93463dcc22848875ca63
farpetrad <farpetrad@gmail.com>
2022-03-17 17:34:53 -0700

    Stub out tests for Home and About

---
+++ OurVueApp/ClientApp/views/About.spec.ts
@@ -0,0 +1,19 @@
+﻿import { mount, VueWrapper } from '@vue/test-utils'
+import About from './About.vue';
+
+describe('It tests the About page', () => {
+    let wrapper: VueWrapper<any>;
+
+    beforeEach(() => {
+        wrapper = mount(About);
+    });
+
+    afterEach(() => {
+        wrapper.unmount();
+    });
+
+    it('Renders the page', () => {
+        expect(wrapper).not.toBeNull();
+        expect(wrapper.exists()).toBeTruthy();
+    });
+});
---
+++ OurVueApp/ClientApp/views/Home.spec.ts
@@ -0,0 +1,19 @@
+﻿import { mount, VueWrapper } from '@vue/test-utils'
+import Home from './Home.vue';
+
+describe('It tests the home page', () => {
+    let wrapper: VueWrapper<any>;
+
+    beforeEach(() => {
+        wrapper = mount(Home);
+    });
+
+    afterEach(() => {
+        wrapper.unmount();
+    });
+
+    it('Renders the page', () => {
+        expect(wrapper).not.toBeNull();
+        expect(wrapper.exists()).toBeTruthy();
+    });
+});
